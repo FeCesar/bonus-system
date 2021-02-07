fe13e4899174363968bc3a363367e924f540d4e6
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-07 20:52:39 -0300

    Envio de Mensagem Funcionando

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import openpyxl as op
-import twilio as tw
+from twilio.rest import Client
+
+account_sid = "ACc6bbdd588bb30e1c81b68e8aa95c659c"
+auth_token = "69792ae172842a10d1172990d8c4fe59"
+client = Client(account_sid, auth_token)
 
 mouthsList = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
@@ -15,3 +19,11 @@ for mouth in mouthsList:
         sell = columns.loc[sellColumn > 55000, 'Vendas'].values[0]
 
         print("No mês de {}, {} fez R${} em vendas!".format(mouth, seller, sell))
+
+        message = client.messages.create(
+            to="+5518996220090",
+            from_="+17474002884",
+            body="No mês de {}, {} fez R${} em vendas!".format(mouth, seller, sell)
+        )
+
+        print(message.sid)
