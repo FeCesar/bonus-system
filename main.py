ce1433ad6c0d762df5952ff991b3ea3512c26403
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-07 20:37:51 -0300

    Coletando informações sobre os funcionarios que atinjiram a meta

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import pandas as pd
 import openpyxl as op
 import twilio as tw
 
-mouthsList = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho']
+mouthsList = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mouth in mouthsList:
     mouthName = "database/" + mouth + ".xlsx"
-    print(mouthName)
+    columns = pd.read_excel(mouthName)
+    sellColumn = columns['Vendas']
+
+    if(sellColumn > 55000).any():
+        
+        seller = columns.loc[sellColumn > 55000, 'Vendedor'].values[0]
+        sell = columns.loc[sellColumn > 55000, 'Vendas'].values[0]
+
+        print("No mês de {}, {} fez R${} em vendas!".format(mouth, seller, sell))
