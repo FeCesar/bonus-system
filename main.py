74f3abc458eda69245b3c0917349ed4595afa017
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-02-07 20:06:58 -0300

    Fazendo nome personalizado dos meses

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import openpyxl as op
 import twilio as tw
+
+mouthsList = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho']
+
+for mouth in mouthsList:
+    mouthName = "database/" + mouth + ".xlsx"
+    print(mouthName)
